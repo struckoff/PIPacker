49c96b9dbd4b0cb5cac3ff0028647366a7b333eb
Alex Struckov <as5423.ru@gmail.com>
2018-07-08 05:22:10 +0300

    add tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,13 @@
-from main import TOKEN
+from main import TOKEN, updater
 import pytest
 
 import requests
 
 class TestTelegramConnection:
-    def test_updater(self):
+    def test_token(self):
         response = requests.get(f'https://api.telegram.org/bot{TOKEN}/getMe')
         print(response.text)
         assert(response.ok == True)
         assert(response.json()['ok'] == True)
+    def test_updater(self):
+        assert(updater.running)
